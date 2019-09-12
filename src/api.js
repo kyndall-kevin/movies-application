b8b7d6c95cf787ee3392b93883b47a7cd723bc09
Kyndall Sims <sims.kyndall22@gmail.com>
2019-09-12 16:16:28 -0500

    final commit for today

--- src/api.js
+++ src/api.js
@@ -1,31 +1,39 @@
 module.exports = {
-    getMovies: () => {
-        return fetch('/api/movies')
-            .then(response => response.json());
-    },
-    addMovie: (data) => {
-        return fetch('/api/movies', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(function (r) {
-            console.log(r);
-        })
-    },
-    editMovie: (id, data) => {
-        return fetch(`/api/movies/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-    },
-    getMovie: (id) => {
-        return fetch(`/api/movies/${id}`)
-            .then(response => response.json());
-    }
+  getMovies: () => {
+    return fetch('/api/movies')
+        .then(response => response.json());
+  },
+  addMovie: (data) => {
+    return fetch('/api/movies', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(function (r) {
+      console.log(r);
+    })
+  },
+  editMovie: (id, data) => {
+    return fetch(`/api/movies/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  },
+  getMovie: (id) => {
+    return fetch(`/api/movies/${id}`)
+        .then(response => response.json());
+  },
+  deleteMovie: (id) => {
+    return fetch(`/api/movies/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
 };
 
