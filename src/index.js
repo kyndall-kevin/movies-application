f857d47c73d494f40a88de2f71bda5c4641da179
Kyndall Sims <sims.kyndall22@gmail.com>
2019-09-12 12:01:36 -0500

    first part started

--- src/index.js
+++ src/index.js
@@ -7,14 +7,45 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const {getMovies, addMovie, editMovie, getMovie} = require('./api.js');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+const loaded = () => {
+  getMovies().then((movies) => {
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+      $('#load').hide();
+      $('#movie-con').append(`<p>id#${id} - ${title} - rating: ${rating}<button class='edit-btn' id="${id}">edit</button></p>`)
+    });
+  }).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
   });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+};
+loaded();
+$('#submit').click(function () {
+  // e.preventDefault();
+  let newMovie = {
+    title: $('#movie-name').val(),
+    rating: $('#movie-rating').val()
+  };
+  addMovie(newMovie).then(() => {
+    $('#load').show();
+    $('#movie-con').html('');
+    loaded();
+  });
+  // console.log(newMovie);
 });
+$('.edit-btn').click(function () {
+  let editid = $(this).attr('id');
+  console.log(editid);
+  getMovie(editid).then(movie =>{
+    $('#edit-movie').val(movie.title);
+    $('#edit-rating').val(movie.rating);
+  })
+});
+
+//When the form is submitted, the page should not reload / refresh,
+// instead, your javascript should make a POST request to
+// /api/movies with the information the user put into the form
+
+//add getMovies in the function using jquery?
